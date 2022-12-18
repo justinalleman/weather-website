33a5f928fe5701d071512b6c175031fd91f9cd63
Justin Alleman <jualleman@merkleinc.com>
2022-12-18 11:18:01 -0600

    add forecast data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,10 @@
 const request = require('request')
 
+// update forecast string to include new data
+// commit changes
+// push to github/heroku
+// test work
+
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=3900f0f1b1f79afab75c64497845c2d6&query=' + latitude + ',' + longitude + '&units=f'
 
@@ -9,7 +14,9 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees out. It feels like " + body.current.feelslike+ " degrees out.")
+            console.log('here is your body', body)
+            callback(undefined,
+                body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees out. It feels like " + body.current.feelslike+ " degrees out. It is currently " + body.location.localtime + " in " + body.location.name + " and the timezone is " + body.location.timezone_id)
         }
     })
 }
