6d79c4ca43c8c453d163fd71a037dc642f9eda89
Justin Alleman <jualleman@merkleinc.com>
2022-12-18 11:07:31 -0600

    add about text

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,6 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    // const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
     const url = 'http://api.weatherstack.com/current?access_key=3900f0f1b1f79afab75c64497845c2d6&query=' + latitude + ',' + longitude + '&units=f'
 
     request({ url, json: true }, (error, { body }) => {
@@ -11,7 +10,6 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to find location', undefined)
         } else {
             callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees out. It feels like " + body.current.feelslike+ " degrees out.")
-            // callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
